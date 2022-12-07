82b901f979f3f4dcecccfa5c770063d9fc76880a
pabloTokuo <contatopbo@hotmail.com>
2022-12-07 12:27:14 -0300

    Java Studies

---
+++ ReservaHotel/src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args)  {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+//        Tratamento de Excessões com Try Catch e criando uma classe DomainException
+        try {
+            System.out.print("Room number: ");
+            int room = sc.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            Date checkin = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkout = sdf.parse(sc.next());
+
+            Reservation reservation = new Reservation(room, checkin, checkout);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkin = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkout = sdf.parse(sc.next());
+
+            reservation.updateDates(checkin, checkout);
+//            Tratando parse Exception
+        } catch (ParseException e) {
+            System.out.println("Invalid date format");
+        } catch (DomainException e) {
+            System.out.println("Error in reservation: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Unexpected error!");
+        }
+
+        sc.close();
+    }
+}
